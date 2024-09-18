d31d2f030d03bf754b6ee02994cb5fb485f68140
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-09-18 16:07:47 +0100

    cli functional

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use std::{env, process};
+use clap::{Parser, Subcommand};
+use std::{
+    fs,
+    string::String,
+};
 
-use teeny_compiler::Config;
+mod lexer;
+mod parser;
+mod emitter;
+
+#[derive(Parser, Debug)]
+#[command(name = "teeny compiler", version, about = "Simple compiler for a BASIC-like grammar into C", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+#[command(version, about, long_about = None)]
+enum Command {
+    /// Compile a single .tiny file
+    #[command()]
+    Compile { path: String },
+}
 
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let args = Cli::parse();
+    let target_dir = "./tinycode/";
 
-    if let Err(e) = teeny_compiler::run(config) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    };
+    match args.command {
+        Command::Compile { path } => {
+            let input = fs::read_to_string(path).unwrap();
+            println!("{}", input);
+            let lex_out = lexer::lex(&input).unwrap();
+            let mut token_iterator = lexer::TokenIterator::new(&lex_out).peekable();
+            let parse_out = parser::parse(&mut token_iterator).unwrap();
+            let parser::AST::Program(statements) = parse_out;
+            let output = emitter::emit_program(statements).unwrap();
+            for line in output {
+                println!("{}", line);
+            }
+        }
+    }
 }
